44a2fe4b5828fb60180b5bdc57e69eb076b3320b
xy <exwaiiii@gmail.com>
2023-07-18 12:38:51 +1000

    started chapter 1

---
+++ chapter 2 building abstractions with data/2.1 intro to data abstraction/2.1.rkt
@@ -0,0 +1,27 @@
+#lang sicp
+
+;Exercise 2.1.  Define a better version of make-rat that handles both positive and negative arguments. Make-rat should normalize the sign so that if the rational number is positive, both the numerator and denominator are positive, and if the rational number is negative, only the numerator is negative.
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (if (>= (* n d) 0) (cons (abs (/ n g)) (abs (/ d g)))
+      (cons (* -1 (abs (/ n g))) (abs (/ d g)))
+    )
+    
+  )
+)
+
+
+(gcd -10 2)
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+(print-rat (make-rat 2 4))
