a54841aebb128f7468e446b4b8cdf5868eee01a5
xy <exwaiiii@gmail.com>
2023-07-18 18:03:31 +1000

    2.3

---
+++ chapter 2 building abstractions with data/2.1 intro to data abstraction/2.3.rkt
@@ -0,0 +1,9 @@
+#lang sicp
+
+; Exercise 2.3.  Implement a representation for rectangles in a plane. (Hint: You may want to make use of exercise 2.2.) In terms of your constructors and selectors, create procedures that compute the perimeter and the area of a given rectangle. Now implement a different representation for rectangles. Can you design your system with suitable abstraction barriers, so that the same perimeter and area procedures will work using either representation?
+
+(define (make-point x y) (cons x y))
+(define (x-point p) (car p))
+(define (y-point p) (cdr p))
+
+(define ())
